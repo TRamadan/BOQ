f109fe9b6c9562bf078ffc1970a377c202d356d5
Ios version <mohammed.magdy.ali.96@gmail.com>
2018-09-02 14:30:56 +0200

    conflict Resolved
    Signed-off-by: Amr Taha <>

--- 9ecomv3/src/pages/tabs/tabs.ts
+++ 9ecomv3/src/pages/tabs/tabs.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
-<<<<<<< .merge_file_a04896
-import { IonicPage, NavController, NavParams , MenuController } from 'ionic-angular';
-import { Cart } from '../../providers/cart/cart';
-=======
 import { IonicPage, NavController, NavParams , MenuController} from 'ionic-angular';
 import { Cart } from '../../providers/cart/cart';
 import { Keyboard } from "@ionic-native/keyboard";
 
->>>>>>> .merge_file_a08080
 
 /**
  * Generated class for the Tabs tabs.
@@ -18,12 +13,8 @@ import { Keyboard } from "@ionic-native/keyboard";
 @IonicPage()
 @Component({
   selector: 'page-tabs',
-<<<<<<< .merge_file_a04896
-  templateUrl: 'tabs.html'
-=======
   templateUrl: 'tabs.html', 
   providers: [Keyboard]
->>>>>>> .merge_file_a08080
 })
 export class TabsPage {
   
@@ -31,26 +22,13 @@ export class TabsPage {
   tab2Root: string = 'HotoffersPage';
   tab3Root: string = 'MyCartPage';
   tab4Root: string = 'SearchPage';
-<<<<<<< .merge_file_a04896
-  tab5Root: string = 'ProfilePage';
-=======
   tab5Root: string = 'ProfilePage'; 
 
   public valueforngif : boolean = true;
->>>>>>> .merge_file_a08080
 
   detail: NavParams;
   selectedIndex: number;
   cart: Cart;
-<<<<<<< .merge_file_a04896
-  constructor(public navCtrl: NavController, private params: NavParams,public menuCtrl : MenuController) {
-    this.menuCtrl.enable(true);
-    this.selectedIndex = params.get('tabIndex') || 0;
-    this.detail = params;
-    this.cart = Cart.getInstance();
-   
-  }
-=======
   constructor(public kb : Keyboard ,   public navCtrl: NavController, private params: NavParams,public menuCtrl : MenuController) {
     this.menuCtrl.enable(true);
     this.selectedIndex = params.get('tabIndex') || 0;
@@ -73,7 +51,6 @@ export class TabsPage {
     
     })
 } 
->>>>>>> .merge_file_a08080
 
   public navTo(tabIndex: any){
     this.selectedIndex=tabIndex;
